83afb7ffc33509ad7730109c398940dd722a705b
Wrangler117708 <184028718@qq.com>
2018-04-17 09:36:34 +0800

    second commit

---
+++ BIM_test.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+
+height = 1.75
+weight = 80.5
+BMI = 26.3
+if BMI < 18.5:
+    print('too smail')
+elif BMI < 25:
+    print('OK')
+elif BMI < 28:
+    print('too weight')
+elif BMI < 32:
+    print('too too weight')
+else:
+    print('very weight')
